3b109c45672b1a9eb138958fe160a0ed9502c341
Vivek Bansal <bansalvivek79@gmail.com>
2015-11-03 18:13:28 +0530

    Create trie.cpp

---
+++ trie.cpp
@@ -0,0 +1,6 @@
+spoj QN01- how to find maximum xor subarray and find the range of it
+codechef REBXOR 
+acm archive question - find max xor subarray 2009 regionals 
+http://www.spoj.com/status/QN01,screw_1011/
+
+spoj phonelist - digits trie simple implementation 
